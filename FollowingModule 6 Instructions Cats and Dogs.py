9947e5ae354d8ce7cdaf64520a02e85382fd72b0
jamiejwil1973 <jwilliams744@ivytech.edu>
2026-07-15 22:39:20 -0400

    Add files via upload

---
+++ FollowingModule 6 Instructions Cats and Dogs.py
@@ -0,0 +1,7 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> c='cats'
+>>> d='dogs'
+>>> s=f"It is raining {c} and {d}."
+>>> print(s)
+It is raining cats and dogs.
